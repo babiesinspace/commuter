611b82adcf042650df050304d57ffbf77387ad07
babiesinspace <allie.g.cooper@gmail.com>
2018-06-01 19:14:50 -0400

    migrate new tables, change direction to better name

--- app/jobs/direction.rb
+++
@@ -1,9 +0,0 @@
-class DirectionJob < ApplicationJob
-  @queue = :get_directions
-
-  def self.perform
-    occurring_today = Commute.where(next_reminder_date: Date.today.all_day)
-    occurring_today.each {|c| c.generate_daily }
-  end
-
-end
