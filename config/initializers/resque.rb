8a3dc71f1b95cf805d633aa848886ba5d5859574
Alexandra Cooper <allie.g.cooper@gmail.com>
2018-06-05 17:12:28 -0400

    maybe?

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,6 +4,8 @@ require 'resque/scheduler/server'
 
 uri = URI.parse(ENV["REDIS_URL"])
 Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+Resque::Scheduler.dynamic = true
+Resque.schedule = YAML.load_file('config/resque_schedule.yml')
 
 
 Resque.after_fork = proc {
