6b0585977a2cda77b49c86395176e362a5978115
Alexandra Cooper <allie.g.cooper@gmail.com>
2018-06-06 01:30:39 -0400

    don't overwrite devise the lazy way

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:phonenumber, location_attributes: [:address]])
-	devise_parameter_sanitizer.permit(:account_update, keys: [:phonenumber, location_attributes: [:address]])  	
-  end
-
 end
