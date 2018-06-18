76c192dd3d96196f13be5a5b3cce202fe4ef5639
babiesinspace <allie.g.cooper@gmail.com>
2018-06-18 12:57:12 -0400

    remove reference to deleted .yml

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -5,7 +5,7 @@ require 'resque/scheduler/server'
 uri = URI.parse(ENV["REDIS_URL"])
 Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 Resque::Scheduler.dynamic = true
-Resque.schedule = YAML.load_file('config/resque_schedule.yml')
+# Resque.schedule = YAML.load_file('config/resque_schedule.yml')
 
 
 Resque.after_fork = proc {
